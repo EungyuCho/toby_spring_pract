9493c1f2ca822dbe72ea5bbb882832ff9a2ea853
unknown <cho2304@naver.com>
2020-02-09 23:48:09 +0900

    1장 오브젝트와 의존관계 : 연결횟수 카운팅 클래스 구현

--- src/main/java/com/toby/pract1/DConnectionMaker.java
+++ src/main/java/com/toby/pract1/DConnectionMaker.java
@@ -9,9 +9,10 @@ public class DConnectionMaker implements ConnectionMaker{
 	@Override
 	public Connection makeConnection() throws ClassNotFoundException, SQLException {
 		//D사일 경우의 DB라고 가정
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection c = DriverManager.getConnection(
-				"jdbc:mysql://127.0.0.1:3306/mydb", "root", "1234");
+//				"jdbc:mysql://127.0.0.1:3306/mydb", "root", "1234");
+				"jdbc:mysql://127.0.0.1:3306/mydb?serverTimezone=UTC", "root", "5180");
 		return c;
 	}
 
--- src/main/java/com/toby/pract1/NConnectionMaker.java
+++ src/main/java/com/toby/pract1/NConnectionMaker.java
@@ -8,10 +8,10 @@ public class NConnectionMaker implements ConnectionMaker{
 
 	@Override
 	public Connection makeConnection() throws ClassNotFoundException, SQLException {
-		//N사일 경우의 DB라고 가정
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection c = DriverManager.getConnection(
-				"jdbc:mysql://127.0.0.1:3306/mydb", "root", "1234");
+//				"jdbc:mysql://127.0.0.1:3306/mydb", "root", "1234");
+				"jdbc:mysql://127.0.0.1:3306/mydb?serverTimezone=UTC", "root", "5180");
 		return c;
 	}
 
