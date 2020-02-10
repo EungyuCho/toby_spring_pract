5b6eac5df0e53369e01935876bf4726ee95e2428
choeungyu <cho2304@naver.com>
2020-02-10 15:08:03 +0900

    2장 테스트 : 포괄적인 테스트

--- src/main/java/com/toby/pract1/UserDao.java
+++ src/main/java/com/toby/pract1/UserDao.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+
 public class UserDao {
 	private ConnectionMaker connectionMaker;
 	private DataSource datasource;
@@ -38,18 +40,42 @@ public class UserDao {
 		PreparedStatement ps = c.prepareStatement(
 				"select * from users where id = ?");
 		ps.setString(1, id);
+		User user = null;
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) {
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+		}
+		rs.close();
+		ps.close();
+		c.close();
 		
+		if(user == null) throw new EmptyResultDataAccessException(1);
+		
+		return user;
+	}
+	
+	public void deleteAll() throws SQLException{
+		Connection c = datasource.getConnection();
+		PreparedStatement ps = c.prepareStatement("delete from users");
+		ps.executeUpdate();
+		ps.close();
+		c.close();
+	}
+	
+	public int getCount() throws SQLException{
+		Connection c = datasource.getConnection();
+		PreparedStatement ps = c.prepareStatement("select count(*) from users");
 		ResultSet rs = ps.executeQuery();
 		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+		int count = rs.getInt(1);
 		
 		rs.close();
 		ps.close();
 		c.close();
 		
-		return user;
+		return count;
 	}
 }
