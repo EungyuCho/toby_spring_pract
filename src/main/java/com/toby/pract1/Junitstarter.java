8a83bf3b06c195818f2be6a3d033cb7bf1c352b0
choeungyu <cho2304@naver.com>
2020-02-10 14:26:47 +0900

    2장 테스트 : JUnit 테스트

---
+++ src/main/java/com/toby/pract1/Junitstarter.java
@@ -0,0 +1,9 @@
+package com.toby.pract1;
+
+import org.junit.runner.JUnitCore;
+
+public class Junitstarter {
+	public static void main(String[] args) {
+		JUnitCore.main("com.toby.pract1.UserDaoTest");
+	}
+}
---
+++ src/test/java/com/toby/pract1/UserDaoTest.java
@@ -0,0 +1,32 @@
+package com.toby.pract1;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class UserDaoTest {
+
+	@Test
+	public void addAndGet() throws SQLException, ClassNotFoundException{
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		User user = new User();
+		user.setId("gyumee");
+		user.setName("박성철");
+		user.setPassword("springno1");
+		
+		dao.add(user);
+		
+		User user2 = dao.get(user.getId());
+		
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+	}
+}
