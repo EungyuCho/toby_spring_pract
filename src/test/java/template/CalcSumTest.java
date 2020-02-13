6df012d87e7847f9db97aec94c804bbafea1cde7
choeungyu <cho2304@naver.com>
2020-02-13 10:19:29 +0900

    3장 : 템플릿 : 계산기 예제 2 템플릿 콜백 적용

--- src/test/java/template/CalcSumTest.java
+++ src/test/java/template/CalcSumTest.java
@@ -5,15 +5,26 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import template.Calculator;
 
 public class CalcSumTest {
+	Calculator calculator;
+	String numFilePath;
+	
+	@Before
+	public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilePath = getClass().getResource("numbers.txt").getPath();
+	}
 	@Test
 	public void sumOfNumbers() throws IOException{
-		Calculator calculator  = new Calculator();
-		int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
-		assertThat(sum, is(10));
+		assertThat(calculator.calcSum(this.numFilePath), is(10));
+	}
+	@Test
+	public void multiplyOfNumber() throws IOException{
+		assertThat(calculator.calcMultiply(this.numFilePath), is(24));
 	}
 }
