cc013cd9a73df9e6be51ed02f967504487093479
choeungyu <cho2304@naver.com>
2020-02-13 12:39:55 +0900

    3장 : 템플릿 : jdbcTemplate를 이용한 UserDao 수정 1

--- src/main/java/com/toby/pract1/UserDao.java
+++ src/main/java/com/toby/pract1/UserDao.java
@@ -1,76 +1,77 @@
 package com.toby.pract1;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
-	private DataSource datasource;
-	private JdbcContext jdbcContext;
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
 	
 	public void setDataSource(DataSource dataSource) {
-		this.jdbcContext = new JdbcContext();
-		this.jdbcContext.setDataSource(dataSource);
-		this.datasource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.dataSource = dataSource;
 	}
 	public void add(final User user) throws ClassNotFoundException, SQLException{
-		this.jdbcContext.workWithStatementStrategy(
-				new StatementStrategy() {
-					public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-						PreparedStatement ps = c.prepareStatement(
-								"insert into users(id, name, password) values(?,?,?)");
-						ps.setString(1, user.getId());
-						ps.setString(2, user.getName());
-						ps.setString(3, user.getPassword());
-						return ps;
+		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
+				user.getId(), user.getName(), user.getPassword());
+	}
+
+	
+	public User get(String id) throws ClassNotFoundException, SQLException{
+		return this.jdbcTemplate.queryForObject("select * from users where id =?", 
+				new Object[] {id},
+				new RowMapper<User> (){
+					@Override
+					public User mapRow(ResultSet rs, int rowNum) throws SQLException{
+						User user = new User();
+						user.setId(rs.getString("id"));
+						user.setName(rs.getString("name"));
+						user.setPassword(rs.getString("password"));
+						return user;
 					}
 				}
 			);
 	}
 	
-	
-	public User get(String id) throws ClassNotFoundException, SQLException{
-//		Connection c = connectionMaker.makeConnection();
-		Connection c = datasource.getConnection();
-		PreparedStatement ps = c.prepareStatement(
-				"select * from users where id = ?");
-		ps.setString(1, id);
-		User user = null;
-		ResultSet rs = ps.executeQuery();
-		if(rs.next()) {
-			user = new User();
-			user.setId(rs.getString("id"));
-			user.setName(rs.getString("name"));
-			user.setPassword(rs.getString("password"));
-		}
-		rs.close();
-		ps.close();
-		c.close();
-		
-		if(user == null) throw new EmptyResultDataAccessException(1);
-		
-		return user;
+	public List<User> getAll() {
+		return this.jdbcTemplate.query("select * from users order by id", 
+				new RowMapper<User>() {
+					@Override
+					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+						User user = new User();
+						user.setId(rs.getString("id"));
+						user.setName(rs.getString("name"));
+						user.setPassword(rs.getString("password"));
+						return user;
+					}
+		});
 	}
 	
 	public void deleteAll() throws SQLException{
-		this.jdbcContext.executeSql("delete from users");
+		this.jdbcTemplate.update("delete from users");
 	}
 	public int getCount() throws SQLException{
-		Connection c = datasource.getConnection();
-		PreparedStatement ps = c.prepareStatement("select count(*) from users");
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		int count = rs.getInt(1);
 		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		return count;
+		/*
+		 * 이전에 작성한 템플릿 메서드 타입 query은 템플릿 안에서 동작하며 첫번째 psc는 sql을 콜백해주고 두번째 resultset은 결과데이터를 콜백함)
+		return this.jdbcTemplate.query(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				return con.prepareStatement("select count(*) from users");
+			}
+		}, new ResultSetExtractor<Integer>() {
+			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException{
+				rs.next();
+				return rs.getInt(1);
+			}
+		});
+		*/
+		return this.jdbcTemplate.queryForInt("select count(*) from users");
 	}
 }
