96a0f2888e894a6c87188c39f9b171b11ee35dbe
choeungyu <cho2304@naver.com>
2020-02-21 16:38:22 +0900

    6장 AOP : Advisor를 통한 Proxy 지정 Method 부가기능 적용

--- src/test/java/com/toby/pract1/DynamicProxyTest.java
+++ src/test/java/com/toby/pract1/DynamicProxyTest.java
@@ -9,6 +9,8 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import Handler.UppercaseHandler;
 import etc.Hello;
@@ -36,7 +38,22 @@ public class DynamicProxyTest {
 		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
 		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
 		assertThat(proxiedHello.saythankYou("Toby"), is("THANK YOU TOBY"));
+	}
+	
+	@Test
+	public void pointcutAdvisor() {
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		pfBean.setTarget(new HelloTarget());
 		
+		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+		pointcut.setMappedName("sayH*");
+		
+		pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+		
+		Hello proxiedHello = (Hello) pfBean.getObject();
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.saythankYou("Toby"), is("Thank You Toby"));
 	}
 	
 	static class UppercaseAdvice implements MethodInterceptor{
@@ -47,5 +64,6 @@ public class DynamicProxyTest {
 			return ret.toUpperCase();
 		}
 	}
+	
 	
 }
