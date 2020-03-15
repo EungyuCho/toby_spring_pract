c05396c9120a5504e01eb3003ce3ee20982f3629
unknown <cho2304@naver.com>
2020-03-15 17:58:24 +0900

    7장 스프링 핵심 기술의 응용 : JAXB 구현 테스트

---
+++ src/test/java/com/toby/pract1/OrmTest.java
@@ -0,0 +1,41 @@
+package com.toby.pract1;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.XmlMappingException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import sqlService.jaxb.SqlType;
+import sqlService.jaxb.Sqlmap;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/OxmTest-content.xml")
+public class OrmTest {
+	@Autowired Unmarshaller unmarshaller;
+	
+	@Test
+	public void unmarshallerSqlMap() throws XmlMappingException, IOException{
+		Source xmlSource = new StreamSource(getClass().getResourceAsStream("sqlmap.xml"));
+		
+		Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(xmlSource);
+		
+		List<SqlType> sqlList = sqlmap.getSql();
+		assertThat(sqlList.size(), is(6));
+
+		assertThat(sqlList.get(1).getKey(), is("userGet"));
+		assertThat(sqlList.get(3).getValue(), is("delete from users"));
+		
+	}
+}
